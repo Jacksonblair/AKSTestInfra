5fd8d22b4d90149a0ed127d3407f5f62001eefe2
Jacksonblair <jackson.blair@live.com>
2024-12-05 08:33:34 +1100

    testing artifacts

--- main.tf
+++ main.tf
@@ -19,41 +19,41 @@ resource "random_id" "acr_name" {
   byte_length = 8
 }
 
-# Create the resource group
-resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
-}
-
-# Create the container registry
-resource "azurerm_container_registry" "example" {
-  name                     = "acr${random_id.acr_name.hex}"
-  location                 = azurerm_resource_group.example.location
-  resource_group_name      = azurerm_resource_group.example.name
-  sku                      = "Basic"
-  admin_enabled            = true
-}
-
-# Create the AKS cluster
-resource "azurerm_kubernetes_cluster" "example" {
-  name                = "test-aks-cluster"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  dns_prefix          = "myaks"
-
-  default_node_pool {
-    name       = "default"
-    node_count = 3
-    vm_size    = "Standard_DS2_v2"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-}
+# # Create the resource group
+# resource "azurerm_resource_group" "example" {
+#   name     = var.resource_group_name
+#   location = var.resource_group_location
+# }
+# 
+# # Create the container registry
+# resource "azurerm_container_registry" "example" {
+#   name                     = "acr${random_id.acr_name.hex}"
+#   location                 = azurerm_resource_group.example.location
+#   resource_group_name      = azurerm_resource_group.example.name
+#   sku                      = "Basic"
+#   admin_enabled            = true
+# }
+# 
+# # Create the AKS cluster
+# resource "azurerm_kubernetes_cluster" "example" {
+#   name                = "test-aks-cluster"
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+#   dns_prefix          = "myaks"
+# 
+#   default_node_pool {
+#     name       = "default"
+#     node_count = 3
+#     vm_size    = "Standard_DS2_v2"
+#   }
+# 
+#   identity {
+#     type = "SystemAssigned"
+#   }
+# }
 
 # Output the kube_config for the AKS cluster
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config
-  sensitive = true
-}
+# output "kube_config" {
+#   value = azurerm_kubernetes_cluster.example.kube_config
+#   sensitive = true
+# }
